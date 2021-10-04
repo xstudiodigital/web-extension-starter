04544c57b758f8abcd605fdcdb2a7ae0a4eb014c
Pedro Jefferson <pedrojefferson.developer@gmail.com>
2021-10-04 15:15:19 -0300

    Update eslint, prettier and typescript configs

--- src/Background/index.ts
+++ src/Background/index.ts
@@ -1,6 +1,8 @@
 import 'emoji-log';
-import {browser} from 'webextension-polyfill-ts';
+
+import { browser } from 'webextension-polyfill-ts';
 
 browser.runtime.onInstalled.addListener((): void => {
+  // eslint-disable-next-line no-console
   console.emoji('🦄', 'extension installed');
 });
